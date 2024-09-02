6c5083c56b739ba500972beffd3a12e1a972f567
Jim <jimchang@thingnario.com>
2024-09-02 12:02:21 +0800

    chore: add pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getEnvPath } from './config/utils';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: getEnvPath(),
       load: [],
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            singleLine: true,
+            translateTime: true,
+          },
+        },
+        customProps: (req, res) => ({
+          context: 'HTTP',
+        }),
+        serializers: {
+          req: (req) => ({
+            url: req.url,
+          }),
+          res: (res) => ({
+            statusCode: res.statusCode,
+          }),
+        },
+      },
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
