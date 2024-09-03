bb47f46dea9d2a60ce14d110228865a796164f53
Jim <jimchang@thingnario.com>
2024-09-03 16:59:08 +0800

    feat: add token to constants

--- src/middleware/header-validation.middleware.ts
+++ src/middleware/header-validation.middleware.ts
@@ -5,6 +5,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { AUTH_TOKEN } from '../constants';
 
 @Injectable()
 export class HeadersValidationMiddleware implements NestMiddleware {
@@ -16,6 +17,10 @@ export class HeadersValidationMiddleware implements NestMiddleware {
       throw new UnauthorizedException(
         'Missing mandatory header: authorization',
       );
+    }
+
+    if (authorization !== `Bearer ${AUTH_TOKEN}`) {
+      throw new ForbiddenException('Invalid authorization token');
     }
 
     if (!requestId) {
