a915a99556aad188917d161a325937f926d7af63
Jim <jimchang@thingnario.com>
2024-09-10 14:39:43 +0800

    feat: add charging station entity

--- src/oscp/oscp.module.ts
+++ src/oscp/oscp.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OscpController } from './adapter/in/oscp.controller';
+import { ChargingStationEntity } from './adapter/out/entities/charging-station.entity';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([ChargingStationEntity])],
   controllers: [OscpController],
 })
 export class OscpModule {}
