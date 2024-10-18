d3e2b13e3f1e8f764077866193a933c500fc2029
ccc <ccchen@thingnario.com>
2024-10-18 22:45:46 +0800

    feat(real-time-data): get transformer real-time data from Redis
    [DREAMS-891]

--- src/elmo/application/real-time-data/real-time-data.service.ts
+++ src/elmo/application/real-time-data/real-time-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { RedisHelper } from '../../adapter/out/redis/redis-helper';
-import { ChargingStationRealTimeData } from './types';
+import { ChargingStationRealTimeData, TransformerRealTimeData } from './types';
 
 @Injectable()
 export class RealTimeDataService {
@@ -21,5 +21,42 @@ export class RealTimeDataService {
 
   buildChargingStationRedisKey(chargingStationUid: string): string {
     return `elmo:charging_station:${chargingStationUid}`;
+  }
+
+  async getTransformerRealTimeData(
+    transformerUid: string,
+  ): Promise<TransformerRealTimeData> {
+    const redisKey = this.buildTransformerRedisKey(transformerUid);
+    const jsonData = await this.redisHelper.getJsonDataFromString(redisKey);
+
+    return {
+      uid: transformerUid,
+      time_mark: new Date(jsonData?.time_mark) ?? null,
+      ac_power_meter_output_kw: jsonData?.ac_power_meter_output_kw ?? null,
+      ac_power_meter_output_kvar: jsonData?.ac_power_meter_output_kvar ?? null,
+      ac_power_meter_output_kva: jsonData?.ac_power_meter_output_kva ?? null,
+      ac_power_meter_output_pf: jsonData?.ac_power_meter_output_pf ?? null,
+      ac_power_meter_freq: jsonData?.ac_power_meter_freq ?? null,
+      ac_power_meter_line_amps_a: jsonData?.ac_power_meter_line_amps_a ?? null,
+      ac_power_meter_line_amps_b: jsonData?.ac_power_meter_line_amps_b ?? null,
+      ac_power_meter_line_amps_c: jsonData?.ac_power_meter_line_amps_c ?? null,
+      ac_power_meter_line_volts_a_b:
+        jsonData?.ac_power_meter_line_volts_a_b ?? null,
+      ac_power_meter_line_volts_b_c:
+        jsonData?.ac_power_meter_line_volts_b_c ?? null,
+      ac_power_meter_line_volts_c_a:
+        jsonData?.ac_power_meter_line_volts_c_a ?? null,
+      ac_power_meter_output_kwh: jsonData?.ac_power_meter_output_kwh ?? null,
+      ac_power_meter_output_kvarh:
+        jsonData?.ac_power_meter_output_kvarh ?? null,
+      ac_power_meter_output_kvah: jsonData?.ac_power_meter_output_kvah ?? null,
+      ac_power_meter_input_kwh: jsonData?.ac_power_meter_input_kwh ?? null,
+      ac_power_meter_input_kvarh: jsonData?.ac_power_meter_input_kvarh ?? null,
+      ac_power_meter_input_kvah: jsonData?.ac_power_meter_input_kvah ?? null,
+    };
+  }
+
+  buildTransformerRedisKey(transformerUid: string): string {
+    return `elmo:transformer:${transformerUid}`;
   }
 }
--- src/elmo/application/real-time-data/types.ts
+++ src/elmo/application/real-time-data/types.ts
@@ -3,3 +3,35 @@ export type ChargingStationRealTimeData = {
   time_mark: Date | null;
   kw: number | null;
 };
+
+export type TransformerRealTimeData = {
+  uid: string;
+  time_mark: Date | null;
+
+  // 即時輸出電功率
+  ac_power_meter_output_kw: number | null; // 實功率
+  ac_power_meter_output_kvar: number | null; // 虛功率
+  ac_power_meter_output_kva: number | null; // 視在功率
+  ac_power_meter_output_pf: number | null; // 功率因數
+  ac_power_meter_freq: number | null; // 頻率
+
+  // 線電流 A
+  ac_power_meter_line_amps_a: number | null; // 線電流 a
+  ac_power_meter_line_amps_b: number | null; // 線電流 b
+  ac_power_meter_line_amps_c: number | null; // 線電流 c
+
+  // 線電壓 V
+  ac_power_meter_line_volts_a_b: number | null; // 線電壓 a-b
+  ac_power_meter_line_volts_b_c: number | null; // 線電壓 b-c
+  ac_power_meter_line_volts_c_a: number | null; // 線電壓 c-a
+
+  // 歷史輸出電量
+  ac_power_meter_output_kwh: number | null; // 歷史輸出有效電能
+  ac_power_meter_output_kvarh: number | null; // 歷史輸出無效電能
+  ac_power_meter_output_kvah: number | null; // 歷史輸出視在電能
+
+  // 歷史輸入電量
+  ac_power_meter_input_kwh: number | null; // 歷史輸入有效電能
+  ac_power_meter_input_kvarh: number | null; // 歷史輸入無效電能
+  ac_power_meter_input_kvah: number | null; // 歷史輸入視在電能
+};
