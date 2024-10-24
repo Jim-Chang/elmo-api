7a369bdf8aedfb267d4966dc2255182527728bc7
Jim <jimchang@thingnario.com>
2024-10-24 11:59:13 +0800

    refactor: add null type to fileds in es raw data

--- src/elmo/application/history-data/charging-station-history-data-service/types.ts
+++ src/elmo/application/history-data/charging-station-history-data-service/types.ts
@@ -1,7 +1,7 @@
 export type ChargingStationFifteenMinuteESRawData = {
   time_mark: string; // 使用 ISO time_mark
 
-  kw: number; // 15 分鐘內平均出來的負載功率
+  kw: number | null; // 15 分鐘內平均出來的負載功率
 };
 
 export type ChargingStationOneHourESRawData =
@@ -10,6 +10,6 @@ export type ChargingStationOneHourESRawData =
 export type ChargingStationOneDayESRawData = {
   time_mark: string; // 使用 ISO time_mark
 
-  kwh: number; // 當日內負載電量
-  life_kwh_total: number; // 當日 concentrated 的最後一筆總電量, 生命週期總負載電量(不斷累積)
+  kwh: number | null; // 當日內負載電量
+  life_kwh_total: number | null; // 當日 concentrated 的最後一筆總電量, 生命週期總負載電量(不斷累積)
 };
--- src/elmo/application/history-data/transformer-history-data-service/types.ts
+++ src/elmo/application/history-data/transformer-history-data-service/types.ts
@@ -3,23 +3,23 @@ export type TransformerFifteenMinuteESRawData = {
   time_mark: string; // 使用 ISO time_mark
 
   // 即時輸出電功率 (平均值，每 15 分鐘計算一次)
-  ac_power_meter_output_kw: number; // 功率
-  ac_power_meter_output_kvar: number; // 虛功
-  ac_power_meter_output_kva: number; // 視在功率
-  ac_power_meter_output_pf: number; // 功率因數
+  ac_power_meter_output_kw: number | null; // 功率
+  ac_power_meter_output_kvar: number | null; // 虛功
+  ac_power_meter_output_kva: number | null; // 視在功率
+  ac_power_meter_output_pf: number | null; // 功率因數
 
   // 頻率 (平均值，每 15 分鐘計算一次)
-  ac_power_meter_freq: number; // 頻率
+  ac_power_meter_freq: number | null; // 頻率
 
   // 線電流 A (平均值，每 15 分鐘計算一次)
-  ac_power_meter_line_amps_a: number; // A
-  ac_power_meter_line_amps_b: number; // B
-  ac_power_meter_line_amps_c: number; // C
+  ac_power_meter_line_amps_a: number | null; // A
+  ac_power_meter_line_amps_b: number | null; // B
+  ac_power_meter_line_amps_c: number | null; // C
 
   // 線電壓 V (平均值，每 15 分鐘計算一次)
-  ac_power_meter_line_volts_a_b: number; // A-B
-  ac_power_meter_line_volts_b_c: number; // B-C
-  ac_power_meter_line_volts_c_a: number; // C-A
+  ac_power_meter_line_volts_a_b: number | null; // A-B
+  ac_power_meter_line_volts_b_c: number | null; // B-C
+  ac_power_meter_line_volts_c_a: number | null; // C-A
 };
 
 export type TransformerOneHourESRawData = TransformerFifteenMinuteESRawData;
@@ -29,17 +29,17 @@ export type TransformerOneDayESRawData = {
   time_mark: string; // 使用 ISO time_mark
 
   // 當日最後一筆 扣掉 當日第一筆 (輸入電量)
-  ac_power_meter_kwh: number; // Δ 輸入有效電能
-  ac_power_meter_kvarh: number; // Δ 輸入無效電能
-  ac_power_meter_kvah: number; // Δ 輸入視在電能
+  ac_power_meter_kwh: number | null; // Δ 輸入有效電能
+  ac_power_meter_kvarh: number | null; // Δ 輸入無效電能
+  ac_power_meter_kvah: number | null; // Δ 輸入視在電能
 
   // 歷史輸出電量 (取最後一筆)
-  ac_power_meter_output_kwh: number; // 有效電能
-  ac_power_meter_output_kvarh: number; // 無效電能
-  ac_power_meter_output_kvah: number; // 輸出視在電能
+  ac_power_meter_output_kwh: number | null; // 有效電能
+  ac_power_meter_output_kvarh: number | null; // 無效電能
+  ac_power_meter_output_kvah: number | null; // 輸出視在電能
 
   // 歷史輸入電量 (取最後一筆)
-  ac_power_meter_input_kwh: number; // 有效電能
-  ac_power_meter_input_kvarh: number; // 無效電能
-  ac_power_meter_input_kvah: number; // 輸入視在電能
+  ac_power_meter_input_kwh: number | null; // 有效電能
+  ac_power_meter_input_kvarh: number | null; // 無效電能
+  ac_power_meter_input_kvah: number | null; // 輸入視在電能
 };
