d68d793f5f13b12996fd5e7566e81001ea26c16a
k7yu <mdyu1000@gmail.com>
2024-10-24 10:59:49 +0800

    chore: assign to variable

--- src/database/factories/feed-line.factory.ts
+++ src/database/factories/feed-line.factory.ts
@@ -2,11 +2,12 @@ import { Factory } from '@mikro-orm/seeder';
 import { FeedLineEntity } from '../../elmo/adapter/out/entities/feed-line.entity';
 import { faker } from '@faker-js/faker';
 
+const FEED_LINES = ['SV61', 'UL15', 'WF37', 'B526'];
 export class FeedLineFactory extends Factory<FeedLineEntity> {
   model = FeedLineEntity;
   definition() {
     return {
-      name: faker.helpers.arrayElement(['SV61', 'UL15', 'WF37', 'B526']),
+      name: faker.helpers.arrayElement(FEED_LINES),
     };
   }
 }
