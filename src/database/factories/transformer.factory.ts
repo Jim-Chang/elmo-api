3ead01a23cd1aa1b7116690a11b0765fe8382f14
SimonYu <simonyu@thingnario.com>
2024-10-25 09:24:22 +0800

    build: update seed

--- src/database/factories/transformer.factory.ts
+++ src/database/factories/transformer.factory.ts
@@ -8,6 +8,10 @@ export class TransformerFactory extends Factory<TransformerEntity> {
     return {
       uid: faker.string.uuid(),
       name: faker.lorem.word(),
+      capacity: faker.number.int({ min: 1000, max: 3000 }),
+      voltageLevel: faker.number.int({ min: 22800, max: 22800 }),
+      tpclid: faker.string.uuid(),
+      group: faker.lorem.word(),
     };
   }
 }
