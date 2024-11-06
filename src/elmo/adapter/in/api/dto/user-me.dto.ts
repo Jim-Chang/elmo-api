b2ca21e240c444ab00746cd1edc2c9af1c0e44af
ccc <ccchen@thingnario.com>
2024-11-06 10:28:04 +0800

    feat(auth): adjust response data of PATCH /api/user/me/password
    [DREAMS-918]

--- src/elmo/adapter/in/api/dto/user-me.dto.ts
+++ src/elmo/adapter/in/api/dto/user-me.dto.ts
@@ -1,5 +1,6 @@
 import { createZodDto } from '@anatine/zod-nestjs';
 import { z } from 'zod';
+import { AccessToken } from '../../../../application/auth/types';
 
 const UserChangePasswordSchema = z.object({
   old_password: z.string().min(1),
@@ -8,4 +9,12 @@ const UserChangePasswordSchema = z.object({
 
 export class UserChangePasswordDto extends createZodDto(
   UserChangePasswordSchema,
+) {}
+
+const UserChangePasswordResponseSchema = z.object({
+  access_token: AccessToken,
+});
+
+export class UserChangePasswordResponseDto extends createZodDto(
+  UserChangePasswordResponseSchema,
 ) {}
